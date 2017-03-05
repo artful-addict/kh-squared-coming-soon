6aef20a93c4180bce1c5504409d69f0ba0a85516
Artful Addict <akinskevin86@gmail.com>
2017-03-04 18:48:01 -0800

    Finished the coming-soon page

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
+//// Variables
+
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
-
 var browserSync = require('browser-sync').create();
-
 var useref = require('gulp-useref');
-
-// Other requires...
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
-
 var cssnano = require('gulp-cssnano');
-
 var imagemin = require('gulp-imagemin');
-
 var cache = require('gulp-cache');
-
 var del = require('del');
-
 var runSequence = require('run-sequence');
 
-gulp.task('sass', function(){
-  return gulp.src('build/assets/scss/**/*.scss')
-  .pipe(sass()) // Gets all files ending with .scss in app/scss and children dirs
-  .pipe(gulp.dest('build/assets/css'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
+//// Task Runner
+
+gulp.task('sass', function() {
+  return gulp.src('build/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('browserSync', function(){
+gulp.watch('build/scss/**/*.scss', ['sass']);
+
+gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: 'build'
     }
   })
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('build/scss/styles.scss', ['sass']);
+  gulp.watch('build/*.html', browserSync.reload);
+  gulp.watch('build/behaviors/**/*.js', browserSync.reload);
 });
 
 gulp.task('useref', function(){
@@ -43,24 +45,23 @@ gulp.task('useref', function(){
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
-}); // run 'gulp useref'
+});
 
 gulp.task('images', function(){
-  return gulp.src('build/assets/images/**/*.+(png|jpg|gif|svg)')
+  return gulp.src('build/images/**/*.+(png|jpg|gif|svg)')
     .pipe(cache(imagemin({
-      // Setting interlaced to true
       interlaced: true
     })))
     .pipe(gulp.dest('dist/images'))
 });
 
-gulp.task('fonts', function() {
-  return gulp.src('build/assets/fonts/**/*')
+gulp.task('fonts', function(){
+  return gulp.src('build/fonts/**/*')
     .pipe(gulp.dest('dist/fonts'))
-}); // run 'gulp fonts'
+});
 
 gulp.task('clean:dist', function(){
   return del.sync('dist');
-}); // run 'gulp clean:dist'
+});
 
-gulp.task('build', ['sass', 'browserSync','useref']);
+gulp.task('build', ['sass', 'browserSync']);
